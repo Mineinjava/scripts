c35553d853aa05376ee1d1aea219c2ae6ff27163
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2025-10-07 18:45:14 -0700

    format

--- cos_tab_gen.py
+++ cos_tab_gen.py
@@ -8,10 +8,12 @@ cos_domain = [0, (1 << DOMAIN_BITS) - 1]
 cos_range_delta = cos_range[1] - cos_range[0]
 cos_domain_delta = cos_domain[1] - cos_domain[0]
 
+
 def function(x):
     return cos(x)
 
-numbers = [function((x * cos_range_delta / cos_domain_delta + min(cos_range))) for x in range(cos_domain[1])]
+
+numbers = [function((x * cos_range_delta / cos_domain_delta + min(cos_range)))
+           for x in range(cos_domain[1])]
 
 print(numbers)
-
